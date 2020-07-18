863ed41dfb70c13332a6621297668e9bec5e931b
Alexey Venderov <alexey.venderov@revolut.com>
2020-07-18 10:59:10 +0200

    Create project

---
+++ src/test/kotlin/com/bcgdv/rebalancer/LauncherIT.kt
@@ -0,0 +1,14 @@
+package com.bcgdv.rebalancer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LauncherIT {
+
+    @Test
+    fun contextLoads() {
+    }
+}
