e0da8d04254040cd8dc5fddb4572d863f2d62b66
Alexey Venderov <alexey.venderov@revolut.com>
2020-07-19 15:05:48 +0200

    Add logging to strategy assigner

--- src/main/kotlin/com/bcgdv/rebalancer/strategy/StrategyAssigner.kt
+++ src/main/kotlin/com/bcgdv/rebalancer/strategy/StrategyAssigner.kt
@@ -2,6 +2,7 @@ package com.bcgdv.rebalancer.strategy
 
 import com.bcgdv.rebalancer.customer.Customer
 import com.bcgdv.rebalancer.time.TimeProvider
+import com.bcgdv.rebalancer.utils.loggerFor
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,12 +11,19 @@ class StrategyAssigner(
     private val timeProvider: TimeProvider
 ) {
 
+    private val log = loggerFor<StrategyAssigner>()
+
     /**
      * Will retrieve all strategies on every call. Optimization is outside of the scope of the task.
      */
     fun strategyFor(customer: Customer): Strategy {
         val yearsToRetirement = customer.yearsToRetirementAt(timeProvider.today())
-        return strategiesProvider.findAll()
+        log.debug("customer={}, yearsToRetirement={}", customer.id, yearsToRetirement)
+
+        val assignedStrategy = strategiesProvider.findAll()
             .find { strategy -> strategy.appliesTo(customer.riskLevel, yearsToRetirement) } ?: Strategy.DEFAULT
+        log.info("Assigned strategy={} to customer={}", assignedStrategy.id, customer.id)
+
+        return assignedStrategy
     }
 }
